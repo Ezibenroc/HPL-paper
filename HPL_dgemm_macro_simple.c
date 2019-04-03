301bb59e63eb0425c40485d18bf886bc1c630205
Arnaud Legrand <arnaud.legrand@imag.fr>
2019-04-03 10:33:06 +0200

    Cosmetics

--- HPL_dgemm_macro_simple.c
+++ HPL_dgemm_macro_simple.c
@@ -1,7 +1,6 @@
-#define |\color{colorfuncall}HPL\_dgemm|(layout, TransA, TransB,                \
-        M, N, K, alpha, A, lda, B, ldb, beta, C, ldc) ({ \
-    double expected_time = (1.029e-11)*((double)M)*      \
-                   ((double)N)*((double)K) + 1.981e-12;  \
-    if(expected_time > 0)                                \
-        |\color{colorfuncall}smpi\_execute\_benched|(expected_time);             \
+#define |\color{colorfuncall}HPL\_dgemm|(layout, TransA, TransB,                       \
+        M, N, K, alpha, A, lda, B, ldb, beta, C, ldc) ({        \
+    double expected_time = (1.029e-11)*((double)M)*             \
+                   ((double)N)*((double)K) + 1.981e-12;         \
+    if(expected_time > 0)  |\color{colorfuncall}smpi\_execute\_benched|(expected_time); \
 })
